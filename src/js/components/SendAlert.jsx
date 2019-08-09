0030594c929745bc615419b3b2f366b916651875
charles-m-doan <cmatthewdoan@gmail.com>
2019-08-09 15:08:51 -0400

    building out very basic message sending with hard-coded values.

--- src/js/components/SendAlert.jsx
+++ src/js/components/SendAlert.jsx
@@ -2,14 +2,53 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default class SendAlert extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alertMessage: {
+        senderId: 3,
+        body: "I need help ASAP!",
+        level: "high",
+        location: { latitude: 0, longitude: 0 }
+      }
+    };
+  }
+
+  addNewMessageFetch = event => {
+    const { alertMessage } = this.state;
+    event.preventDefault();
+    console.log("Message submitted!");
+
+    fetch("http://localhost:8080/api/messages", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(alertMessage)
+    });
+  };
+
+  updateBody = event => {
+    this.setState({
+      alertMessage: {
+        senderId: this.state.alertMessage.senderId,
+        body: event.target.value,
+        level: "high",
+        location: { latitude: 0, longitude: 0 }
+      }
+    });
+  };
+
   render() {
     return (
       <>
-        <form>
+        <form onSubmit={this.addNewMessageFetch}>
           <button className="alert-button__send">
-            <Link to="/main">
+            <h1>send alert</h1>
+            {/* <Link to="/main">
               <h1>send alert</h1>
-            </Link>
+            </Link> */}
           </button>
           <section className="input-panel">
             <h3 className="field-label">Help Message:</h3>
@@ -19,6 +58,7 @@ export default class SendAlert extends React.Component {
               defaultValue="I need help ASAP!"
               contentEditable="true"
               className="field-value"
+              onChange={this.updateBody}
             />
             <h3 className="field-label">Urgency Level:</h3>
             <select name="urgency">
