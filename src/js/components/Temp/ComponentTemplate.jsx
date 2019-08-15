6b15d6507821cdf4dbd8c30e85ecd1464eb7b9ea
charles-m-doan <cmatthewdoan@gmail.com>
2019-08-15 09:35:52 -0400

    Building out ComponentTemplate.

---
+++ src/js/components/Temp/ComponentTemplate.jsx
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import * as API from "../../state/API";
+
+class ComponentTemplate extends Component {
+	_isMounted = false;
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			redirectFlags: { login: false, main: false },
+			redirectPaths: { login: "/login", main: "/main" }
+		};
+	}
+
+	render() {
+		if (this.shouldRedirect()) {
+			return <Redirect to={this.getRedirectPath()} />;
+		}
+
+		return (
+			<React.Fragment>
+				<h1>ComponentTemplate</h1>
+				<h2>Most Components Should Copy this Template.</h2>
+			</React.Fragment>
+		);
+	}
+
+	componentDidMount() {
+		this._isMounted = true;
+		this.setLoginRedirect();
+
+		// Make Async Fetch Calls Below. In "then" statement, check "_isMounted" before updating this.state.
+		let dataPromise; //= fetchCall(); dataPromise.then((data)=>{ if(_isMounted){ setState({something: data}) } }) etc...
+	}
+
+	componentWillUnmount() {
+		this._isMounted = false;
+	}
+
+	userIsNotLoggedIn() {
+		return this.props.loggedInUser.id === -1;
+	}
+
+	setLoginRedirect() {
+		if (this.userIsNotLoggedIn()) {
+			this.setState({ redirectFlags: { login: true } });
+		} else {
+			this.setState({ redirectFlags: { login: false } });
+		}
+	}
+
+	shouldRedirect() {
+		if (this.state.redirectFlags.login == true) {
+			return true;
+		}
+		if (this.state.redirectFlags.main == true) {
+			return true;
+		}
+		return false;
+	}
+
+	getRedirectPath() {
+		if (this.state.redirectFlags.login == true) {
+			return this.state.redirectPaths.login;
+		}
+		if (this.state.redirectFlags.main == true) {
+			return this.state.redirectPaths.main;
+		}
+		return false;
+	}
+}
+
+// REDUX BOILERPLATE BELOW ---------------------------
+
+const mapStateToProps = state => {
+	return {
+		loggedInUser: state.loggedInUser
+	};
+};
+
+const mapDispatchToProps = dispatch => {
+	return {
+		setLoggedInUserId: userId => {
+			dispatch({
+				type: "SET_ID",
+				payload: userId
+			});
+		}
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(ComponentTemplate);
--- src/js/components/Temp/TempNav.jsx
+++ src/js/components/Temp/TempNav.jsx
@@ -2,26 +2,29 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 const TempNav = props => {
-  return (
-    <React.Fragment>
-      <h4>(Temporary Nav Links for testing)</h4>
-      <Link to="/login">
-        <h3>LoginForm</h3>
-      </Link>
-      <Link to="/signup">
-        <h3>SignUpForm</h3>
-      </Link>
-      <Link to="/alertform">
-        <h3>AlertForm</h3>
-      </Link>
-      <Link to="/grdians">
-        <h3>AllGrdiansView</h3>
-      </Link>
-      <Link to="/grdians/1">
-        <h3>SingleGrdianView</h3>
-      </Link>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<h4>(Temporary Nav Links for testing)</h4>
+			<Link to="/template">
+				<h3>TEMPLATE</h3>
+			</Link>
+			<Link to="/login">
+				<h3>LoginForm</h3>
+			</Link>
+			<Link to="/signup">
+				<h3>SignUpForm</h3>
+			</Link>
+			<Link to="/alertform">
+				<h3>AlertForm</h3>
+			</Link>
+			<Link to="/grdians">
+				<h3>AllGrdiansView</h3>
+			</Link>
+			<Link to="/grdians/1">
+				<h3>SingleGrdianView</h3>
+			</Link>
+		</React.Fragment>
+	);
 };
 
 export default TempNav;
