038386b31dd4aa42936458e4bb64d664b1a73181
charles-m-doan <cmatthewdoan@gmail.com>
2019-08-16 11:19:50 -0400

    adding ability to add and remove active alerts and grdians.

--- src/App.jsx
+++ src/App.jsx
@@ -15,92 +15,78 @@ import SingleGrdianView from "./js/components/Grdians/SingleGrdianView";
 import SingleAlertView from "./js/components/Alert/SingleAlertView";
 import ComponentTemplate from "./js/components/Temp/ComponentTemplate";
 import AlertFormBrancher from "./js/components/Alert/AlertFormBrancher";
-import NewAlertForm from "./js/components/Alert/NewAlertForm";
-import ActiveAlertForm from "./js/components/Alert/ActiveAlertForm";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-	render() {
-		return (
-			<React.Fragment>
-				<Router>
-					<Layout>
-						<Switch>
-							<Route
-								exact
-								path="/signup"
-								render={props => <SignUpForm {...props} />}
-							/>
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <Layout>
+            <Switch>
+              <Route
+                exact
+                path="/signup"
+                render={props => <SignUpForm {...props} />}
+              />
 
-							<Route
-								exact
-								path="/"
-								render={props => <LoginForm {...props} />}
-							/>
+              <Route
+                exact
+                path="/"
+                render={props => <LoginForm {...props} />}
+              />
 
-							<Route
-								exact
-								path="/login"
-								render={props => <LoginForm {...props} />}
-							/>
+              <Route
+                exact
+                path="/login"
+                render={props => <LoginForm {...props} />}
+              />
 
-							<Route
-								exact
-								path="/main"
-								render={props => <MainView {...props} />}
-							/>
+              <Route
+                exact
+                path="/main"
+                render={props => <MainView {...props} />}
+              />
 
-							<Route
-								exact
-								path="/alertform"
-								render={props => <AlertFormBrancher {...props} />}
-							/>
+              <Route
+                exact
+                path="/alertform"
+                render={props => <AlertFormBrancher {...props} />}
+              />
 
-							<Route
-								exact
-								path="/newalert"
-								render={props => <NewAlertForm {...props} />}
-							/>
+              <Route
+                exact
+                path="/alerts/:id"
+                render={props => <SingleAlertView {...props} />}
+              />
 
-							<Route
-								exact
-								path="/activealert"
-								render={props => <ActiveAlertForm {...props} />}
-							/>
+              <Route
+                exact
+                path="/grdians/:id"
+                render={props => <SingleGrdianView {...props} />}
+              />
 
-							<Route
-								exact
-								path="/alerts/:id"
-								render={props => <SingleAlertView {...props} />}
-							/>
+              <Route
+                exact
+                path="/grdians"
+                render={props => <AllGrdiansView {...props} />}
+              />
 
-							<Route
-								exact
-								path="/grdians/:id"
-								render={props => <SingleGrdianView {...props} />}
-							/>
-
-							<Route
-								exact
-								path="/grdians"
-								render={props => <AllGrdiansView {...props} />}
-							/>
-
-							<Route
-								exact
-								path="/template"
-								render={props => <ComponentTemplate {...props} />}
-							/>
-						</Switch>
-					</Layout>
-				</Router>
-			</React.Fragment>
-		);
-	}
+              <Route
+                exact
+                path="/template"
+                render={props => <ComponentTemplate {...props} />}
+              />
+            </Switch>
+          </Layout>
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
