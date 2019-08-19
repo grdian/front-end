d3f9b7af960abe7d53e12bd3a63e45d68baf6b3b
charles-m-doan <cmatthewdoan@gmail.com>
2019-08-19 12:04:49 -0400

    adding config folder

--- src/js/components/Geolocation/MapMarker.jsx
+++ src/js/components/Geolocation/MapMarker.jsx
@@ -2,37 +2,37 @@ import React, { Component } from "react";
 import { nullUser } from "../../state/API";
 
 class MapMarker extends Component {
-	static defaultProps = {
-		userLabel: nullUser.firstName,
-		imgURL: nullUser.imgURL
-	};
+  static defaultProps = {
+    userLabel: nullUser.firstName,
+    imgURL: nullUser.imgURL
+  };
 
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-	render() {
-		return (
-			<React.Fragment>
-				<div className="marker">
-					{/* <h3 className="marker__label">{this.props.userLabel}</h3> */}
-					<figure className="marker__figure">
-						<img
-							className="marker__figure__img"
-							onError={this.addDefaultSrc}
-							src={"/" + this.props.imgURL}
-							alt="Profile pic"
-						/>
-					</figure>
-				</div>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <div className="marker">
+          {/* <h3 className="marker__label">{this.props.userLabel}</h3> */}
+          <figure className="marker__figure">
+            <img
+              className="marker__figure__img"
+              onError={this.addDefaultSrc}
+              src={"" + this.props.imgURL}
+              alt="Profile pic"
+            />
+          </figure>
+        </div>
+      </React.Fragment>
+    );
+  }
 
-	addDefaultSrc = ev => {
-		ev.target.src = "images/portraits/portrait_genericDefault.jpg";
-	};
+  addDefaultSrc = ev => {
+    ev.target.src = "images/portraits/portrait_genericDefault.jpg";
+  };
 }
 
 export default MapMarker;
